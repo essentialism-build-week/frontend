ac32ee13a75bd238eed5c2d311949b3933985c8f
lyndsiWilliams <kcatgirl@gmail.com>
2020-01-10 11:50:45 -0600

    Fixed more routing

--- essentialism/src/components/Registration.js
+++ essentialism/src/components/Registration.js
@@ -49,7 +49,7 @@ const Registration = () => {
     event.preventDefault();
     setIsFetching(true);
     axiosWithAuth()
-      .post("/auth/register", credentials)
+      .post("/register", credentials)
       .then(response => {
         console.log(response.data);
         response.data.saved === true
